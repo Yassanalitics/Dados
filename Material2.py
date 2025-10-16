45f5fc8e21d4c0182b326e95534fa0bc70eaa3b9
Yassanalitics <Yasminribeiro152005@gmail.com>
2025-10-16 12:31:20 -0300

    Update and rename operLog2.py to Material2.py

--- Material2.py
+++ Material2.py
@@ -17,3 +17,13 @@ c= "bcdfghjklmnpqrstvwx"
 e = "\x20\x0A"
 #sys.stdout.write(c[11]+v[5]+e[0]+c[1]+v[3]+c[2]+v[1])
 sys.stdout.write(v[5]+v[0]+c[14]+c[9]+v[2]+c[10])
+
+#Exercicio04
+import sys
+import array
+
+Array = array.array('q', [2,4,6,7,8,10,11])  
+
+sys.stdout.write( str(Array) )
+sys.stdout.write( "\n" )
+sys.stdout.write( str(Array[3]) )
